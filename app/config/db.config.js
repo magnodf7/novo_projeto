066b29d646801040abb597349e10e0c8b68a77f4
magnodf7 <magno.brunodf7@gmIL.com>
2023-09-15 13:17:12 -0300

    aplicando confuguração de bancos de dados e campo de controles

---
+++ app/config/db.config.js
@@ -0,0 +1,14 @@
+módulo . exportações  =  {
+    HOST : "localhost" ,
+    USUÁRIO : "postgres" ,
+    SENHA : "123" ,
+    Banco de dados : "" ,
+    dialeto : "postgres" ,
+    piscina : {
+       máximo : 5 ,
+       min : 0 ,
+       adquirir : 30.000 ,
+       inativo : 10.000
+    } 
+   } ;
+*/
